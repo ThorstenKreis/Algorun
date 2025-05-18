5c89e0a47aa42497bb19755c74b9f15acc532327
Thorsten Kreis <thorsten.kreis87@gmail.com>
2025-05-18 14:34:29 +0200

    Add files via upload

---
+++ Algorun/test.py
@@ -0,0 +1,12 @@
+zaehler = 10
+nenner = 0
+
+#print(zaehler/nenner)
+
+if nenner and zaehler/nenner:
+    pass
+lst = [1, 2, 3, 4, None]
+
+for i in range(len(lst)):
+    if lst[i]
+    print(lst[i])
