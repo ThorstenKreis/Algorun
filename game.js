ee1f5f637b98f19896c8169c1c0fdbf24ffec3fb
Thorsten Kreis <thorsten.kreis87@gmail.com>
2025-05-22 13:35:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 import * as commands from './commands.js';
 import { levels } from './levels.js';
-import { setupDragAndDrop } from './drag.js';
 
 
 const gameRoot = document.getElementById("game-root");
